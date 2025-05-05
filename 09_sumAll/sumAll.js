77f488b48598546e55d798545267a71e03406504
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-06 01:18:04 +0800

    solved sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+    if (firstInt > lastInt) {
+        let temp = firstInt;
+        firstInt = lastInt;
+        lastInt = temp;
+    }
 
+    if(!Number.isInteger(firstInt) || !Number.isInteger(lastInt) ) return "ERROR";
+    if (firstInt < 0 || lastInt < 0) return "ERROR";
+
+    let totalSum = 0;
+
+    for(let i = firstInt; i <= lastInt; i++) {
+        totalSum += i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
