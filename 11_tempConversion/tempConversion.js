b51e6307852152d302636915bc3941de3192b4ef
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-06 01:59:38 +0800

    solved temp conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = (temperature - 32) * (5/9);
+  if (celsius%1 !== 0){
+    celsius = Number(celsius.toFixed(1));
+  }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let farenheit = temperature * (9/5) + 32;
+  if (farenheit%1 !== 0){
+    farenheit = Number(farenheit.toFixed(1));
+  }
+  return farenheit;
 };
 
 // Do not edit below this line
