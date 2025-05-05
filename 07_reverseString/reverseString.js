1b3f8df3c44be23336243987c652c55481029bad
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-05 23:03:07 +0800

    solved reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringLength = string.length;
+    let reversedString = "";
 
+    for (i = 1; i <= stringLength; i++) {
+        reversedString += string.charAt(stringLength-i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
